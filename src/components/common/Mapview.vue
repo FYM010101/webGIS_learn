5829e2850e5f3c0ff97a98cac0f60e7486cf2b35
FYM010101 <814116576@qq.com>
2022-05-07 14:58:25 +0800

    增加ArcGIS JS API,实例化地图

---
+++ src/components/common/Mapview.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="mapview"></div>
+</template>
+
+<script>
+import { loadModules } from 'esri-loader';
+
+const options = {
+    url: 'https://js.arcgis.com/4.23/',
+    css: 'https://js.arcgis.com/4.23/esri/themes/light/main.css',
+};
+
+export default {
+    name: 'Map-view',
+    components: {},
+    mounted: function () {
+        this._createMapView();
+    },
+    methods: {
+        async _createMapView() {
+            const [Map, MapView] = await loadModules(['esri/Map', 'esri/views/MapView'], options);
+
+            const map = new Map({
+                basemap: 'osm',
+            });
+
+            const view = new MapView({
+                container: 'mapview',
+                map: map,
+                zoom: 10,
+                center: [104.071595,30.674956],
+            });
+
+            view.ui.components = [];
+
+            console.log(view);
+        },
+    },
+};
+</script>
+
+<style>
+#mapview {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+</style>
